d2b0eb78e7e14354d0ed5bc4ee38d55a4ff36933
Zhuangh7 <zhuanghq2007@foxmail.com>
2018-08-14 11:23:38 +0800

    fix_session_issue&add more features

---
+++ DataAnalytics/Utils/SummaryUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAnalytics.Utils
+{
+    public class SummaryUtil
+    {
+        public static string[] readSymbols()
+        {
+            return null;
+        }
+        public static summary readSummary(string symbol)
+        {
+            return new summary();
+        }
+    }
+}
