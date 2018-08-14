83f78f743910ef7b422eaf7ad8fe7779e3867dcb
zxp <994471527@qq.com>
2018-08-14 16:28:50 +0800

    complete signin and signup. and add portfolioID in portfolio class

--- DataAnalytics/Utils/PortfolioUtil.cs
+++ DataAnalytics/Utils/PortfolioUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,11 +11,21 @@ namespace DataAnalytics.Utils {
         public static bool _savePortfolio(string username,portfolio portfolio) {
             return true;
         }
+
         public static object _readPortfolioDetail(portfolio port)
         {
             return new { errmsg = "login time out" };
         }
+
         public static portfolio[] _readPortFolio(string username) {
+            var conn = new SqlConnection(@"server=.\sqlexpress02;database=DataAnalytics;integrated security=true");
+            var cmd = new SqlCommand("get_UserPwd", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", username);
+            conn.Open();
+            SqlDataReader resultReader = cmd.ExecuteReader();
+            //未完待续
+            conn.Close();
             return null;
         }
     }
--- DataAnalytics/Utils/UserUtil.cs
+++ DataAnalytics/Utils/UserUtil.cs
@@ -8,24 +8,53 @@ using System.Web;
 namespace DataAnalytics.Utils {
     public class UserUtil {
         public static bool userSignIn(string username,string password) {
-            string pwd;
+            string pwd = "";
             var conn = new SqlConnection(@"server=.\sqlexpress02;database=DataAnalytics;integrated security=true");
-            var cmd = new SqlCommand("get_allSymbols", conn);
+            var cmd = new SqlCommand("get_UserPwd", conn);
             cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", username);
             conn.Open();
             SqlDataReader resultReader = cmd.ExecuteReader();
-            int i = 0;
             while (resultReader.Read())
             {
-                symbols.Add(resultReader.GetValue(1).ToString());
-                i++;
+                pwd = resultReader.GetValue(0).ToString();
+            }
+            conn.Close();
+            if (pwd.Equals(password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool userSignUp(string username,string password) {
+            var conn = new SqlConnection(@"server=.\sqlexpress02;database=DataAnalytics;integrated security=true");
+            var cmd = new SqlCommand("save_User", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", username);
+            cmd.Parameters.AddWithValue("@pwd", password);
+            conn.Open();
+            try
+            {
+                int result = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (result == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (SqlException)
+            {
+                return false;
             }
 
-            conn.Close();
-            return symbols.ToArray();
-        }
-        public static bool userSignUp(string username,string password) {
-            return username == "user" && password == "pass";
         }
     }
 }
